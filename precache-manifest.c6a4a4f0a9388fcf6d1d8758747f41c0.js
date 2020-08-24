65fa091e448c56047f91659aa1367b0d57fb57f9
kajisun <papapahatena@yahoo.co.jp>
2020-08-24 20:20:29 +0900

    Updates

---
+++ precache-manifest.c6a4a4f0a9388fcf6d1d8758747f41c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7034020d99450861362eb200b41881",
+    "url": "/pomodoro_clock/index.html"
+  },
+  {
+    "revision": "c8a52e68f751639ccfc8",
+    "url": "/pomodoro_clock/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc396d9f03831121e1b3",
+    "url": "/pomodoro_clock/static/js/2.f4596141.chunk.js"
+  },
+  {
+    "revision": "f5eeca88c65df516af9519a8bb5412f5",
+    "url": "/pomodoro_clock/static/js/2.f4596141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a52e68f751639ccfc8",
+    "url": "/pomodoro_clock/static/js/main.0a5aba6b.chunk.js"
+  },
+  {
+    "revision": "876221ed443e52accde0",
+    "url": "/pomodoro_clock/static/js/runtime-main.e6f5db23.js"
+  }
+]);
